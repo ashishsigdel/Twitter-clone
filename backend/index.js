262371daa8d769d252709a807f3f41128514d408
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-12-07 15:58:47 +0545

    Connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { error } from "console";
 
 dotenv.config();
 
@@ -10,3 +12,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT} !!!`);
 });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongoDB.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
